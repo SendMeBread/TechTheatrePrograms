3fe51b2158de8a75866199e67de47171c34cb73e
SendMeMyBread <joemczero@gmail.com>
2023-09-23 19:52:47 -0400

    Update TEST1.01.sh

--- GPIO_Stuff/Testing/TEST1.01.sh
+++ GPIO_Stuff/Testing/TEST1.01.sh
@@ -1,3 +1,47 @@
 #!/bin/sh
 
+sudo -s
+
 echo "r/R, g/G, y/Y, b/B, o/O, or w/W for ON/OFF for that light..."
+
+while true
+do
+    read -s -n 1 key
+    case $key in
+        r)
+            python3 LED_TEST1.pyw r on
+            ;;
+        R)
+            python3 LED_TEST.pyw r off
+            ;;
+        g)
+            python3 LED_TEST1.pyw g on
+            ;;
+        G)
+            python3 LED_TEST1.pyw g off
+            ;;
+        y)
+            python3 LED_TEST1.pyw y on
+            ;;
+        Y)
+            python3 LED_TEST1.pyw y off
+            ;;
+        b)
+            python3 LED_TEST1.pyw y on
+            ;;
+        B)
+            python3 LED_TEST1.pyw b off
+            ;;
+        o)
+            python3 LED_TEST1.pyw o on
+            ;;
+        O)
+            python3 LED_TEST1.pyw o off
+            ;;
+        w)
+            python3 LED_TEST1.pyw w on
+            ;;
+        W)
+            python3 LED_TEST1.pyw w off
+            ;;
+    esac
