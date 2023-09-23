7e1d63b978ccfd96789e48a116a08c189577eff7
SendMeMyBread <joemczero@gmail.com>
2023-09-23 08:25:02 -0400

    Update GPIO_TPTGS_EM1.py

--- GPIO_Stuff/Electromagnets/GPIO_TPTGS_EM1.py
+++ GPIO_Stuff/Electromagnets/GPIO_TPTGS_EM1.py
@@ -3,16 +3,4 @@ import time
 LED = gpiozero.LED
 pause = time.sleep
 
-red1 = gpiozero.PWMLED(17)
-
-while True:
-    red1.value = 0
-    pause(0.5)
-    red1.value = 0.25
-    pause(0.5)
-    red1.value = 0.5
-    pause(0.5)
-    red1.value = 0.75
-    pause(0.5)
-    red1.value = 1
-    pause(1.25)
+red1 = gpiozero.LED(17)
