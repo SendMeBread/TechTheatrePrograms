f63aee942ea5a7c5aa4324c3c5271575d74bf04b
SendMeMyBread <joemczero@gmail.com>
2023-09-23 15:21:32 -0400

    Update LED_TEST1.py

--- GPIO_Stuff/Electromagnets/LED_TEST1.py
+++ GPIO_Stuff/Electromagnets/LED_TEST1.py
@@ -9,6 +9,8 @@ r = 17
 g = 18
 y = 22
 b = 23
+o = 25
+w = 9
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -19,6 +21,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(y, GPIO.OUT)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(b, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(o, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(w, GPIO.OUT)
 
 if rction=="off":
     if whichled=="r":
@@ -29,11 +35,17 @@ if rction=="off":
         GPIO.output(y, False)
     if whichled=="b":
         GPIO.output(b, False)
+    if whichled=="o":
+        GPIO.output(o, False)
+    if whichled=="w":
+        GPIO.output(w, False)
     if whichled=="all":
         GPIO.output(r, False)
         GPIO.output(g, False)
         GPIO.output(y, False)
         GPIO.output(b, False)
+        GPIO.output(o, False)
+        GPIO.output(w, False)
 if rction=="on":
     if whichled=="r":
         GPIO.output(r, True)
@@ -43,8 +55,14 @@ if rction=="on":
         GPIO.output(y, True)
     if whichled=="b":
         GPIO.output(b, True)
+    if whichled=="o":
+        GPIO.output(o, True)
+    if whichled=="w":
+        GPIO.output(w, True)
     if whichled=="all":
         GPIO.output(r, True)
         GPIO.output(g, True)
         GPIO.output(y, True)
         GPIO.output(b, True)
+        GPIO.output(o, True)
+        GPIO.output(w, True)
