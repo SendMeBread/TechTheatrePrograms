c791cccdd56c7039ec8ba78cbef7135b0b89da98
SendMeMyBread <joemczero@gmail.com>
2023-09-24 14:10:53 -0400

    Update play_sarajevo.sh

--- GPIO_Stuff/LED/play_sarajevo.sh
+++ GPIO_Stuff/LED/play_sarajevo.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-(spotify 1>/dev/null 2>&1 &) && qdbus org.mpris.MediaPlayer2.spotify / org.freedesktop.MediaPlayer2.OpenUri https://open.spotify.com/track/3sBnSBnzpEYKpJYGsAIbup?si=ba67d321046c4e69
+(spotify 1>/dev/null 2>&1 &) && qdbus org.mpris.MediaPlayer2.spotify / org.freedesktop.MediaPlayer2.OpenUri sptify:track:3sBnSBnzpEYKpJYGsAIbup
