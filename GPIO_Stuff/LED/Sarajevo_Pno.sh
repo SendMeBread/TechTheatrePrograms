9311d65bca80b668c4c7ec464b3193594b48b310
SendMeMyBread <joemczero@gmail.com>
2023-09-24 17:03:46 -0400

    Fix crap

--- GPIO_Stuff/LED/Sarajevo_Pno.sh
+++ GPIO_Stuff/LED/Sarajevo_Pno.sh
@@ -3,76 +3,76 @@
 #Piano = Blue: p2, p8, p9, p13;
 
 #Measure 1
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p2 on
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p2 off
 python3 Sarajevo.pyw p8 on
 #Measure 2
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p13 on
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p13 off
 python3 Sarajevo.pyw p2 on
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p8 off
 python3 Sarajevo.pyw p9 on
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p13 off
 python3 Sarajevo.pyw p9 off
 python3 Sarajevo.pyw p8 on
 python3 Sarajevo.pyw p2 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p8 off
 python3 Sarajevo.pyw p9 on
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p8 on
 python3 Sarajevo.pyw p9 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p2 on
 python3 Sarajevo.pyw p8 off
-sleep(0.5)
+sleep 0.5
 python3 Sarajevo.pyw p13 on
 python3 Sarajevo.pyw p9 on
 python3 Sarajevo.pyw p2 off
 #Measure 3
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p8 on
 python3 Sarajevo.pyw p13 off
 python3 Sarajevo.pyw p9 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p2 on
 python3 Sarajevo.pyw p8 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p13 on
 python3 Sarajevo.pyw p9 on
 python3 Sarajevo.pyw p2 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p8 on
 python3 Sarajevo.pyw p13 off
 python3 Sarajevo.pyw p9 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p2 on
 python3 Sarajevo.pyw p8 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p13 on
 python3 Sarajevo.pyw p2 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p8 on
 python3 Sarajevo.pyw p13 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p8 off
 python3 Sarajevo.pyw p9 on
 #Measure 4
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p9 off
 python3 Sarajevo.pyw p8 on
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p13 on
 python3 Sarajevo.pyw p8 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p2 on
 python3 Sarajevo.pyw p13 off
-sleep(.5)
+sleep .5
 python3 Sarajevo.pyw p9 on
 python3 Sarajevo.pyw p2 off
