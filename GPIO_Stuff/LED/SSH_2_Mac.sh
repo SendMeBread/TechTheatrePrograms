9795d188ab9211cec06606b19119ee1d964ec1a4
SendMeMyBread <joemczero@gmail.com>
2023-09-24 20:57:51 -0400

    1114

--- GPIO_Stuff/LED/SSH_2_Mac.sh
+++ GPIO_Stuff/LED/SSH_2_Mac.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/bash
 
 ssh josephmb@192.168.1.44
+
+spotify-terminal.py -u joemczero@gmail.com
+
+xdotool key colon f i n d space C h r i s t m a s space E v e space slash space S a r a j e v o space 1  2 slash 2 4 space b y space T r a n s minus S i b e r i a n space O r c h e s t r a colon
