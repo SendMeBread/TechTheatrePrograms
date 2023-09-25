78f780fa1a1cf111207bc054f3ea904529f64ed6
SendMeMyBread <joemczero@gmail.com>
2023-09-24 22:04:56 -0400

    Update Keybinds.sh

--- GPIO_Stuff/LED/Keybinds.sh
+++ GPIO_Stuff/LED/Keybinds.sh
@@ -7,100 +7,100 @@ do
     read -s -n 1 key
     case $key in
         o)
-            sudo python3 MAX.pyw p1 on
+            sudo python3 Max.pyw p1 on
             ;;
         O)
-            sudo python3 MAX.pyw p1 off
+            sudo python3 Max.pyw p1 off
             ;;
         w)
-            sudo python3 MAX.pyw p2 on
+            sudo python3 Max.pyw p2 on
             ;;
         W)
-            sudo python3 MAX.pyw p2 off
+            sudo python3 Max.pyw p2 off
             ;;
         e)
-            sudo python3 MAX.pyw p3 on
+            sudo python3 Max.pyw p3 on
             ;;
         E)
-            sudo python3 MAX.pyw p3 off
+            sudo python3 Max.pyw p3 off
             ;;
         r)
-            sudo python3 MAX.pyw p4 on
+            sudo python3 Max.pyw p4 on
             ;;
         R)
-            sudo python3 MAX.pyw p4 off
+            sudo python3 Max.pyw p4 off
             ;;
         v)
-            sudo python3 MAX.pyw p5 on
+            sudo python3 Max.pyw p5 on
             ;;
         V)
-            sudo python3 MAX.pyw p5 off
+            sudo python3 Max.pyw p5 off
             ;;
         x)
-            sudo python3 MAX.pyw p6 on
+            sudo python3 Max.pyw p6 on
             ;;
         X)
-            sudo python3 MAX.pyw p6 off
+            sudo python3 Max.pyw p6 off
             ;;
         n)
-            sudo python3 MAX.pyw p7 on
+            sudo python3 Max.pyw p7 on
             ;;
         N)
-            sudo python3 MAX.pyw p7 off
+            sudo python3 Max.pyw p7 off
             ;;
         g)
-            sudo python3 MAX.pyw p8 on
+            sudo python3 Max.pyw p8 on
             ;;
         G)
-            sudo sudo python3 MAX.pyw p8 off
+            sudo python3 Max.pyw p8 off
             ;;
         i)
-            sudo python3 MAX.pyw p9 on
+            sudo python3 Max.pyw p9 on
             ;;
         I)
-            sudo python3 MAX.pyw p9 off
+            sudo python3 Max.pyw p9 off
             ;;
         t)
-            sudo python3 MAX.pyw p10 on
+            sudo python3 Max.pyw p10 on
             ;;
         T)
-            sudo python3 MAX.pyw p10 off
+            sudo python3 Max.pyw p10 off
             ;;
         l)
-            sudo python3 MAX.pyw p11 on
+            sudo python3 Max.pyw p11 on
             ;;
         L)
-            sudo python3 MAX.pyw p11 off
+            sudo python3 Max.pyw p11 off
             ;;
         2)
-            sudo python3 MAX.pyw p12 on
+            sudo python3 Max.pyw p12 on
             ;;
         @)
-            sudo python3 MAX.pyw p12 off
+            sudo python3 Max.pyw p12 off
             ;;
         5)
-            sudo python3 MAX.pyw p13 on
+            sudo python3 Max.pyw p13 on
             ;;
         %)
-            sudo python3 MAX.pyw p13 of
+            sudo python3 Max.pyw p13 of
             ;;
         4)
-            sudo python3 MAX.pyw p14 on
+            sudo python3 Max.pyw p14 on
             ;;
         $)
-            sudo python3 MAX.pyw p14 off
+            sudo python3 Max.pyw p14 off
             ;;
         f)
-            sudo python3 MAX.pyw p15 on
+            sudo python3 Max.pyw p15 on
             ;;
         F)
-            sudo python3 MAX.pyw p15 off
+            sudo python3 Max.pyw p15 off
             ;;
         s)
-            sudo python3 MAX.pyw p16 on
+            sudo python3 Max.pyw p16 on
             ;;
         S)
-            sudo python3 MAX.pyw p16 off
+            sudo python3 Max.pyw p16 off
             ;;
     esac
 done
